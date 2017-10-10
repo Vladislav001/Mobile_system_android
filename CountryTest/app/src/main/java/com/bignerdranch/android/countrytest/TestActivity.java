d7c188e0089dc264d7873be528c7e784b0d575e8
Vladislav <VladGurjev@mail.ru>
2017-10-10 21:20:27 +0400

    Подгруз изображений статичный

--- CountryTest/app/src/main/java/com/bignerdranch/android/countrytest/TestActivity.java
+++ CountryTest/app/src/main/java/com/bignerdranch/android/countrytest/TestActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.countrytest;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
 public class TestActivity extends AppCompatActivity {
+
+    //Android (Java)
+    private StorageReference mStorageRef;
 
     private ImageView mBTNtrue;
     private ImageView mBTNfalse;
@@ -16,11 +26,14 @@ public class TestActivity extends AppCompatActivity {
     private ImageView mBTNstopTest;
     private TextView mTVtitle;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+       // Uri file = Uri.fromFile(new File("manager_buttons/like_dislike/0/dislike.jpg"));
+        StorageReference riversRef = mStorageRef.child("manager_buttons/like_dislike/0/dislike.jpg");
 
         mBTNtrue = (ImageView) findViewById(R.id.btn_true);
         mBTNfalse = (ImageView) findViewById(R.id.btn_false);
@@ -28,6 +41,12 @@ public class TestActivity extends AppCompatActivity {
         mBTNrightArrow = (ImageView) findViewById(R.id.btn_right_arrow);
         mBTNstopTest = (ImageView) findViewById(R.id.btn_stop);
         mTVtitle = (TextView) findViewById(R.id.tv_title);
+
+        mBTNtrue.setImageResource(R.drawable.authorization);
+
+        Picasso.with(getBaseContext()) //передаем контекст приложения
+                .load("https://www.likeni.ru/upload/rk/33f/1024dp90.jpg") //адрес изображения
+                .into(mBTNfalse); //ссылка на ImageView
 
     }
 
