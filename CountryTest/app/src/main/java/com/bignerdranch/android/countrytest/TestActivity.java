58f107184b561948f67c7035ad5d0adc19853a64
Vladislav <VladGurjev@mail.ru>
2017-10-12 13:58:10 +0400

    Подгрузка фото из хранилища

--- CountryTest/app/src/main/java/com/bignerdranch/android/countrytest/TestActivity.java
+++ CountryTest/app/src/main/java/com/bignerdranch/android/countrytest/TestActivity.java
@@ -1,23 +1,29 @@
 package com.bignerdranch.android.countrytest;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
 
-import java.io.File;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity  {
 
     //Android (Java)
     private StorageReference mStorageRef;
+    private DatabaseReference myRef;
 
     private ImageView mBTNtrue;
     private ImageView mBTNfalse;
@@ -26,14 +32,39 @@ public class TestActivity extends AppCompatActivity {
     private ImageView mBTNstopTest;
     private TextView mTVtitle;
 
+    DatabaseReference manageButtonsTrueFale;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-       // Uri file = Uri.fromFile(new File("manager_buttons/like_dislike/0/dislike.jpg"));
-        StorageReference riversRef = mStorageRef.child("manager_buttons/like_dislike/0/dislike.jpg");
+        mStorageRef = FirebaseStorage.getInstance().getReference(); // ссылка на дефолтное Storage
+        myRef = FirebaseDatabase.getInstance().getReference(); // ссылка на дефолтную БД
+
+//
+//        manageButtonsTrueFale = (DatabaseReference) myRef.child("test/manage_buttons").orderByChild("style_images_like_dislike");
+//
+//        //устанавливаем слушатель на узле пользователи (usersRef выступает ссылкой на соответствующий узел)
+//        manageButtonsTrueFale.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                //получаем информацию
+//                String TrueFalse = (String) dataSnapshot.getValue();
+//                Toast.makeText(TestActivity.this, "Значение тру и фалсе = " + TrueFalse, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                //ошибка
+//            }
+//        });
+
+        StorageReference trueLink = mStorageRef.child("/manager_buttons/like_dislike/0/like.png");
+        StorageReference falseLink = mStorageRef.child("/manager_buttons/like_dislike/0/dislike.png");
+        StorageReference NextLink = mStorageRef.child("/manager_buttons/next_back/0/next.png");
+        StorageReference BackLink = mStorageRef.child("/manager_buttons/next_back/0/back.png");
+        StorageReference StopLink = mStorageRef.child("/manager_buttons/stop_test/0/stop.png");
 
         mBTNtrue = (ImageView) findViewById(R.id.btn_true);
         mBTNfalse = (ImageView) findViewById(R.id.btn_false);
@@ -42,12 +73,25 @@ public class TestActivity extends AppCompatActivity {
         mBTNstopTest = (ImageView) findViewById(R.id.btn_stop);
         mTVtitle = (TextView) findViewById(R.id.tv_title);
 
-        mBTNtrue.setImageResource(R.drawable.authorization);
 
-        Picasso.with(getBaseContext()) //передаем контекст приложения
-                .load("https://www.likeni.ru/upload/rk/33f/1024dp90.jpg") //адрес изображения
-                .into(mBTNfalse); //ссылка на ImageView
+
+        setImageFromFB(getBaseContext(), trueLink, mBTNtrue);
+        setImageFromFB(getBaseContext(), falseLink, mBTNfalse);
+        setImageFromFB(getBaseContext(), NextLink, mBTNrightArrow);
+        setImageFromFB(getBaseContext(), BackLink, mBTNlefttArrow);
+        setImageFromFB(getBaseContext(), StopLink, mBTNstopTest);
 
     }
+
+
+
+        // Загрузка изображения из FB Storage
+        public static void setImageFromFB(Context context, StorageReference storageReference, ImageView imageView){
+        Glide.with(context)
+                .using(new FirebaseImageLoader())
+                .load(storageReference)
+                .into(imageView);
+    }
+
 
 }
